d6c08ebb72189a6c2ff06f9dee1f95a2dffd0ee3
2nozha <Anna-Seslavinskaya@mail.ru>
2021-10-15 17:23:40 +0300

    second commit

--- src/main/java/ru/dataart/academy/java/figures/Calculator.java
+++ src/main/java/ru/dataart/academy/java/figures/Calculator.java
@@ -3,8 +3,8 @@ package ru.dataart.academy.java.figures;
 public class Calculator {
     public double Calculator(Figure[] figures){
         double sum=0;
-        for (int f = 0; f < figures.length; f++) {
-            sum=sum+figures[f].Area();
+        for (Figure figure:figures) {
+            sum+=figure.Area();
         }
         return sum;
     }
